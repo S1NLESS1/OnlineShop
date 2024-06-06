59ce7b70a264ace9966af7ebdc9b34c9e3c4ca3f
S1NLESS1 <xryak.uoy@gmail.com>
2024-06-06 13:11:22 +0300

    Create catalog.js

---
+++ js/catalog.js
@@ -0,0 +1,55 @@
+;(function() {
+  var catalogSection = document.querySelector('.products');
+
+  if (catalogSection === null) {
+    return;
+  }
+
+  var removeChildren = function(item) {
+    while (item.firstChild) {
+      item.removeChild(item.firstChild);
+    }
+  };
+
+  var updateChildren = function(item, children) {
+    removeChildren(item);
+    for (var i = 0; i < children.length; i += 1) {
+      item.appendChild(children[i]);
+    }
+  };
+
+  var catalog = catalogSection.querySelector('.catalog');
+  var catalogNav = catalogSection.querySelector('.main_item');
+  var catalogItems = catalogSection.querySelectorAll('.catalog_item');
+
+  catalogNav.addEventListener('click', function(e) {
+    var target = e.target;
+    var item = myLib.closestItemByClass(target, 'catalog_nav_buttons');
+
+    if (item === null || item.classList.contains('is-active')) {
+      return;
+    }
+
+    e.preventDefault();
+    var filterValue = item.getAttribute('data-filter');
+    var previousBtnActive = catalogNav.querySelector('.catalog_nav_buttons.is-active');
+
+    previousBtnActive.classList.remove('is-active');
+    item.classList.add('is-active');
+
+    if (filterValue === 'all') {
+      updateChildren(catalog, catalogItems);
+      return;
+    }
+
+    var filteredItems = [];
+    for (var i = 0; i < catalogItems.length; i += 1) {
+      var current = catalogItems[i];
+      if (current.getAttribute('data-category') === filterValue) {
+        filteredItems.push(current);
+      }
+    }
+
+    updateChildren(catalog, filteredItems);
+  });
+})();
